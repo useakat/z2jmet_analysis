632a763fef8875615214bb41d7770f8ce5aa762d
useakat <yoshi@Yoshitaros-MacBook-Pro.local>
2015-09-30 19:13:00 +0900

    run_br.sh OK; development finished

--- run_br.sh
+++ run_br.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+########### Inputs #########################################
+BRZ=$1
 ########### Parameters #########################################
 analysis=atlas_1503_03290 # signal ATLAS 2lepton jets MET
 #analysis=cms_1502_06031 # 2lepton jets MET
@@ -9,6 +11,7 @@ analysis=atlas_1503_03290 # signal ATLAS 2lepton jets MET
 #analysis=atlas_1403_4853 # 2lepton jets MET (Stop search) no sensitivity
 #analysis=atlas_1407_0583 # no sensitivity
 #analysis=atlas_conf_2013_049 # no sensitivity due to jet veto
+
 exp=atlas # This is not used yet (under construction)
 #exp=cms # This is not used yet (under construction)
 
@@ -16,23 +19,19 @@ mg5dir_zz=../MG5/pp_bpbp~_dzd~z_dlld~vv
 mg5dir_zw=../MG5/pp_bpbp~_dzuw_dllulv
 mg5dir_ww=../MG5/pp_bpbp~_uw-u~w+_ulvu~lv
 
-runext=10k
 mg5_mode=1 # 0:don't generate events, 1:generate events
+runext=10k
 nevents=10000
 
-#results_dir=results_local
-#results_dir=results_local_brz2
 results_dir=results_local_brz3
-#results_dir=results_local_brz_test
 
-MBmin=400
-MBmax=400
+MBmin=500
+MBmax=$MBmin
 dMB=20
 
-BRZ=0.6
-BRzll=0.03365
-BRzvv=0.06667
-BRwlv=10.8
+BRzll=0.06729
+BRzvv=0.2
+BRwlv=0.216
 ################## Main Program ##################################
 rm -rf s.dat
 touch s.dat
@@ -42,28 +41,39 @@ MB=$MBmin
 while [ $MB -le $MBmax ];do
     run_name=m_${MB}_${BRZ}_${runext}
 
-    BRW=`echo "scale=3; 1 -$BRZ" | bc | sed 's/^\./0./'`
+    BRW=`echo "scale=5; 1 -$BRZ" | bc | sed 's/^\./0./'`
 
-    # Hathor NNLO cross section calculation
-    sed -e "s/double mt = .*/double mt = $MB;/" myprogram.cxx > myprogram.tmp
-    mv myprogram.tmp myprogram.cxx
-    make myprogram
-    ./myprogram | tee hathor.log    
-
-    echo $run_name $analysis $exp $MB $mg5dir_zz $nevents zz $results_dir
-    exit
     if [ $mg5_mode -eq 1 ];then
+        # Hathor NNLO cross section calculation
+	sed -e "s/double mt = .*/double mt = $MB;/" myprogram.cxx > myprogram.tmp
+	mv myprogram.tmp myprogram.cxx
+	make myprogram
+	./myprogram | tee hathor.log    
+	
+	# Event generation and analysis with MG5-Pythia-delphes-CheckMate
 	./get_brevents.sh $run_name $analysis $exp $MB $mg5dir_zz $nevents zz $results_dir
 	./get_brevents.sh $run_name $analysis $exp $MB $mg5dir_zw $nevents zw $results_dir
 	./get_brevents.sh $run_name $analysis $exp $MB $mg5dir_ww $nevents ww $results_dir
     fi
 
-    ss_zz=`cat $results_dir/${analysis}_${MB}/s_zz.dat`
-    ss_zw=`cat $results_dir/${analysis}_${MB}/s_zw.dat`
-    ss_ww=`cat $results_dir/${analysis}_${MB}/s_ww.dat`
+    # Obtain event numbers passing analysis cuts for each B' decay mode
+    ss_zz2=(`cat $results_dir/${analysis}_${MB}/s_zz.dat`)
+    ss_zw2=(`cat $results_dir/${analysis}_${MB}/s_zw.dat`)
+    ss_ww2=(`cat $results_dir/${analysis}_${MB}/s_ww.dat`)
 
-    ss=`echo "scale=3; $ss_zz*$BRZ^2*$BRzll*$BRzvv +$ss_zw*$BRZ/0.5*$BRW/0.5*$BRzll*$BRwlv +$ss_ww*$BRW^2*$BRwlv^2" | bc | sed 's/^\./0./'`
+    ss_zz=${ss_zz2[0]}
+    dss_zz=${ss_zz2[1]}
+    ss_zw=${ss_zw2[0]}
+    dss_zw=${ss_zw2[1]}
+    ss_ww=${ss_ww2[0]}
+    dss_ww=${ss_ww2[1]}
+    s95=${ss_zz2[2]}
+
+    # Combine results for overall event numbers passing the analysis cuts
+    ss=`echo "scale=5; $ss_zz*$BRZ^2*$BRzll*$BRzvv +$ss_zw*$BRZ*$BRW*$BRzll*$BRwlv +$ss_ww*$BRW^2*$BRwlv^2" | bc | sed 's/^\./0./'`
+    dss=`echo "scale=5; sqrt($dss_zz^2*$BRZ^4*$BRzll^2*$BRzvv^2 +$dss_zw^2*$BRZ^2*$BRW^2*$BRzll^2*$BRwlv^2 +$dss_ww^2*$BRW^4*$BRwlv^4)" | bc | sed 's/^\./0./'`
     echo $MB $ss >> s.dat
+    echo $MB $ss $dss $s95
 
     MB=`expr $MB + $dMB`
     i=`expr $i + 1`
