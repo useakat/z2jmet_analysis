1a493cfe609ee3ddfca62a30174fb949b01442c7
useakat <yoshi@Yoshitaros-MacBook-Pro.local>
2015-10-04 19:08:34 +0900

    get_analysis_results.py debugged

--- get_analysis_results.py
+++ get_analysis_results.py
@@ -13,20 +13,27 @@ BRW = float(sys.argv[8])
 BRzll = float(sys.argv[9])
 BRzvv = float(sys.argv[10])
 BRwlv = float(sys.argv[11])
+runext = sys.argv[12]
 
 S_zz = []
 dS_zz = []
+dSstat_zz = []
+dSsys_zz = []
 S_zw = []
 dS_zw = []
+dSstat_zw = []
+dSsys_zw = []
 S_ww = []
 dS_ww = []
+dSstat_ww = []
+dSsys_ww = []
 SR = []
 S95obs = []
 S95exp = []
 
-file_zz = results_dir + "/" + analysis + "_" + mass + "/zz/evaluation/" + analysis + "_r_limits.txt"
-file_zw = results_dir + "/" + analysis + "_" + mass + "/zw/evaluation/" + analysis + "_r_limits.txt"
-file_ww = results_dir + "/" + analysis + "_" + mass + "/ww/evaluation/" + analysis + "_r_limits.txt"
+file_zz = results_dir + "/" + analysis + "_" + runext + "_" + mass + "/zz/evaluation/" + analysis + "_r_limits.txt"
+file_zw = results_dir + "/" + analysis + "_" + runext + "_" + mass + "/zw/evaluation/" + analysis + "_r_limits.txt"
+file_ww = results_dir + "/" + analysis + "_" + runext + "_" + mass + "/ww/evaluation/" + analysis + "_r_limits.txt"
 
 ld = open(file_zz)
 lines = ld.readlines()
@@ -37,6 +44,8 @@ for line in lines:
         data = line[:-1].split()
         SR.append(data[0])
         S_zz.append(data[1])
+        dSstat_zz.append(data[2])
+        dSsys_zz.append(data[3])
         dS_zz.append(data[4])
         S95obs.append(data[5])
         S95exp.append(data[6])
@@ -49,6 +58,8 @@ for line in lines:
     if line != '\n' and line.find("dS_stat") < 0:
         data = line[:-1].split()
         S_zw.append(data[1])
+        dSstat_zw.append(data[2])
+        dSsys_zw.append(data[3])
         dS_zw.append(data[4])
 
 ld = open(file_ww)
@@ -58,15 +69,32 @@ for line in lines:
     if line != '\n' and line.find("dS_stat") < 0:
         data = line[:-1].split()
         S_ww.append(data[1])
+        dSstat_ww.append(data[2])
+        dSsys_ww.append(data[3])
         dS_ww.append(data[4])
 
 rmax = -1
 for i in xrange(ncol):
+#    print i
+#    print dSstat_zz[i], dSsys_zz[i], dS_zz[i]
+#    print dSstat_zw[i], dSsys_zw[i], dS_zw[i]
+#    print dSstat_ww[i], dSsys_ww[i], dS_ww[i]
+
     ss = zzfact * float(S_zz[i]) * BRZ**2 * BRzll * BRzvv \
         +zwfact * float(S_zw[i]) * BRZ * BRW * BRzll * BRwlv \
         +wwfact * float(S_ww[i]) * BRW**2 * BRwlv**2
 
-    dss = math.sqrt(zzfact**2 * float(dS_zz[i])**2 * BRZ**4 * BRzll**2 * BRzvv**2 \
+    dss_stat = math.sqrt(zzfact**2 * float(dSstat_zz[i])**2 * BRZ**4 * BRzll**2 * BRzvv**2 \
+              +zwfact**2 * float(dSstat_zw[i])**2 * BRZ**2 * BRW**2 * BRzll**2 * BRwlv**2 \
+              +wwfact**2 * float(dSstat_ww[i])**2 * BRW**4 * BRwlv**4)
+
+    dss_sys = zzfact * float(dSsys_zz[i]) * BRZ**2 * BRzll * BRzvv \
+        +zwfact * float(dSsys_zw[i]) * BRZ * BRW * BRzll * BRwlv \
+        +wwfact * float(dSsys_ww[i]) * BRW**2 * BRwlv**2
+
+    dss = math.sqrt(dss_stat**2 +dss_sys**2)
+
+    dss2 = math.sqrt(zzfact**2 * float(dS_zz[i])**2 * BRZ**4 * BRzll**2 * BRzvv**2 \
               +zwfact**2 * float(dS_zw[i])**2 * BRZ**2 * BRW**2 * BRzll**2 * BRwlv**2 \
               +wwfact**2 * float(dS_ww[i])**2 * BRW**4 * BRwlv**4)
 
@@ -74,9 +102,15 @@ for i in xrange(ncol):
     if r > rmax:
         sr_max = SR[i]
         s_max = ss
+        dsstat_max = dss_stat
+        dssys_max = dss_sys
         ds_max = dss
+        ds_max2 = dss2
         s95obs_max = S95obs[i]
 
-print sr_max, s_max, ds_max, s95obs_max
+        r_max = (s_max -2*ds_max)/float(S95obs[i])
 
+#print "SR", "S", "dS_stat", "dS_sys", "dS_tot", "S95obs", "r"
+print sr_max, s_max, dsstat_max, dssys_max, ds_max, s95obs_max, r_max
+#print ds_max, ds_max2
 
--- run_br.sh
+++ run_br.sh
@@ -7,8 +7,8 @@ run_mode=$3 # 0: don't generate/analysis events
             # 1: don't generate but analysis events
             # 2: generate and analysis events
 ########### Parameters #########################################
-analysis=atlas_1503_03290 # signal ATLAS 2lepton jets MET
-#analysis=cms_1502_06031 # 2lepton jets MET
+#analysis=atlas_1503_03290 # signal ATLAS 2lepton jets MET
+analysis=cms_1502_06031 # 2lepton jets MET
 #analysis=atlas_1405_7875 # ATLAS 2-6 jet +MET
 #analysis=atlas_conf_2013_047 # 2-6 jets +MET
 #analysis=atlas_conf_2013_089 # 2leptons +MET no sensitivity
@@ -16,8 +16,8 @@ analysis=atlas_1503_03290 # signal ATLAS 2lepton jets MET
 #analysis=atlas_1407_0583 # no sensitivity
 #analysis=atlas_conf_2013_049 # no sensitivity due to jet veto
 
-exp=atlas # This is not used yet (under construction)
-#exp=cms # This is not used yet (under construction)
+#exp=atlas # This is not used yet (under construction)
+exp=cms # This is not used yet (under construction)
 
 mg5dir_zz=../MG5/pp_bpbp~_dzd~z_dlld~vv
 mg5dir_zw=../MG5/pp_bpbp~_dzuw_dllulv
@@ -90,29 +90,17 @@ while [ $MB -le $MBmax ];do
     echo    
 
     # Obtain event numbers passing analysis cuts for each B' decay mode
-    results=(`./get_analysis_results.py $results_dir $analysis $MB $zzfact $zwfact $wwfact $BRZ $BRW $BRzll $BRzvv $BRwlv`)
-    ss=${results[1]}
-    dss=${results[2]}
-    s95=${results[3]}
+    results=(`./get_analysis_results.py $results_dir $analysis $MB $zzfact $zwfact $wwfact $BRZ $BRW $BRzll $BRzvv $BRwlv $runext`)
 
-#      ss_zz2=(`cat $results_dir/${analysis}_${runext}_${MB}/s_zz.dat`)
-#      ss_zw2=(`cat $results_dir/${analysis}_${runext}_${MB}/s_zw.dat`)
-#      ss_ww2=(`cat $results_dir/${analysis}_${runext}_${MB}/s_ww.dat`)
-
-#      ss_zz=${ss_zz2[0]}
-#      dss_zz=${ss_zz2[1]}
-#      ss_zw=${ss_zw2[0]}
-#      dss_zw=${ss_zw2[1]}
-#      ss_ww=${ss_ww2[0]}
-#      dss_ww=${ss_ww2[1]}
-#      s95=${ss_zz2[2]}
-
-#      # Combine results for overall event numbers passing the analysis cuts
-#      ss=`echo "scale=5; $zzfact*$ss_zz*$BRZ^2*$BRzll*$BRzvv +$zwfact*$ss_zw*$BRZ*$BRW*$BRzll*$BRwlv +$wwfact*$ss_ww*$BRW^2*$BRwlv^2" | bc | sed 's/^\./0./'`
-#      dss=`echo "scale=5; sqrt($zzfact*$dss_zz^2*$BRZ^4*$BRzll^2*$BRzvv^2 +$zwfact*$dss_zw^2*$BRZ^2*$BRW^2*$BRzll^2*$BRwlv^2 +$wwfact*$dss_ww^2*$BRW^4*$BRwlv^4)" | bc | sed 's/^\./0./'`
+    s=${results[1]}
+    dsstat=${results[2]}
+    dssys=${results[3]}
+    ds=${results[4]}
+    s95=${results[5]}
+    r=${results[6]}
 
     echo $MB $ss >> s.dat
-    echo $MB $ss $dss $s95
+    echo $MB $s $dsstat $dssys $ds $s95 $r
     echo ""
 
     MB=`expr $MB + $dMB`
